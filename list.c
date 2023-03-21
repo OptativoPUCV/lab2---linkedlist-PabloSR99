29ae457bfb1b1df9e89f7b5c7db722b57ffd925d
Replit user <>
2023-03-21 16:01:31 +0000

    new try

--- list.c
+++ list.c
@@ -29,6 +29,10 @@ Node * createNode(void * data) {
 }
 
 List * createList() {
+     List *lista = (List*) malloc(sizeof(List));
+     lista->head=NULL;
+     lista->tail=NULL;
+     lista->current=NULL; 
      return NULL;
 }
 
